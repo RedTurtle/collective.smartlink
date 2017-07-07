a831e3f6953ac8aeee65e101cbb7f8af812cacc8
daniele-andreotti <daniele.andreotti@redturtle.it>
2017-07-07 14:51:18 +0200

    Fixed external link set as required.

--- src/collective/smartlink/browser/form.py
+++ src/collective/smartlink/browser/form.py
@@ -16,9 +16,9 @@ from zope.interface import Invalid
 class BaseForm(object):
 
     def additional_validation(self, data):
-        return
         # Some additional validation
-        if data.get('remoteUrl') and data.get('ISmartLinkExtension.internal_link'):
+        if (not data.get('remoteUrl') and not data.get('ISmartLinkExtension.internal_link')) or \
+            bool(data.get('remoteUrl')) and bool(data.get('ISmartLinkExtension.internal_link')):
             raise WidgetActionExecutionError('remoteUrl',
                 Invalid(_('error_internallink_externallink_doubled', default="You must select an internal link or enter an external link. You cannot have both."),))
 
@@ -30,6 +30,7 @@ class EditForm(BaseForm, DefaultEditForm):
     def updateWidgets(self):
         super(EditForm, self).updateWidgets()
         self.widgets['remoteUrl'].required = False
+        self.fields['remoteUrl'].field.required = False
 
     @button.buttonAndHandler(dmf(u'Save'), name='save')
     def handleApply(self, action):
@@ -59,7 +60,7 @@ class AddForm(BaseForm, DefaultAddForm):
     def updateWidgets(self):
         super(AddForm, self).updateWidgets()
         self.widgets['remoteUrl'].required = False
-        
+        self.fields['remoteUrl'].field.required = False
 
     @button.buttonAndHandler(dmf('Save'), name='save')
     def handleAdd(self, action):
--- src/collective/smartlink/browser/view.py
+++ src/collective/smartlink/browser/view.py
@@ -6,6 +6,15 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 class LinkRedirectView(Base):
     index = ViewPageTemplateFile('templates/link.pt')
 
+    def _url_uses_scheme(self, schemes, url=None):
+        url = url or self.context.remoteUrl
+        if not url:
+            return False
+        for scheme in schemes:
+            if url.startswith(scheme):
+                return True
+        return False
+
     def absolute_target_url(self):
         """Compute the absolute target URL."""
         if self.context.internal_link:
