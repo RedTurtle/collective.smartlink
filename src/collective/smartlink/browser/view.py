e81b836ccc5fd7968a2120082b153af0a3d25270
daniele-andreotti <daniele.andreotti@redturtle.it>
2018-09-27 16:54:45 +0200

    Fixed view when link is broken

--- src/collective/smartlink/browser/view.py
+++ src/collective/smartlink/browser/view.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from plone.app.contenttypes.browser.link_redirect_view import LinkRedirectView as Base
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+from plone import api
+from .. import _
 
 
 class LinkRedirectView(Base):
@@ -17,7 +20,20 @@ class LinkRedirectView(Base):
 
     def absolute_target_url(self):
         """Compute the absolute target URL."""
+        context = self.context
+        mtool = getToolByName(context, 'portal_membership')
+        can_edit = mtool.checkPermission('Modify portal content', context)
+
         if self.context.internal_link:
+            if not self.context.internal_link.to_object:
+                if not can_edit:
+		    api.portal.show_message(
+				    message=_("link_not_found"),
+				    request=self.request,
+				    type='warning')
+                    return self.request.response.redirect(api.portal.get().absolute_url())
+                else:
+                    return
             return self.context.internal_link.to_object.absolute_url()
         else:
             return super(LinkRedirectView, self).absolute_target_url()
