b9934d1b90fd9c83e46d3fcb155c85b9c12dca7f
Giacomo Monari <arsenico13@hotmail.it>
2018-11-08 12:39:14 +0100

    [new][chg] update step (from 1000 to 1100)

---
+++ src/collective/smartlink/upgrades/upgrades.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from plone.dexterity.interfaces import IDexterityFTI
+from zope.component import queryUtility
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+default_profile = 'profile-collective.smartlink:default'
+
+
+def to_1100(context):
+    """
+    """
+    logger.info('Upgrading collective.smartlink to version 1100')
+    fti = queryUtility(IDexterityFTI, name="Link", default=None)
+    if not fti:
+        return
+
+    # Change the behaviors for Link content type
+    # behaviors = [
+    #     it for it in fti.behaviors
+    #     if 'collective.smartlink.behaviors.interfaces.ISmartLinkExtension'
+    #     not in it]
+    #
+    # logger.info('Removing ISmartLinkExtension behavior from Link type.')
+    # behaviors = tuple(set(behaviors))
+    # fti._updateProperty('behaviors', behaviors)
+
+    logger.info('Changing model_file for Link type.')
+    fti._updateProperty('model_file', 'plone.app.contenttypes.schema:link.xml')
