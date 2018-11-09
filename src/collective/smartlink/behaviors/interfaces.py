ef592925bb5552bfd4ec7e768221befe3115aa11
Giacomo Monari <arsenico13@hotmail.it>
2018-11-09 16:26:28 +0100

    [chg] commented out the new field defined in the ISmartLinkExtension behavior

--- src/collective/smartlink/behaviors/interfaces.py
+++ src/collective/smartlink/behaviors/interfaces.py
@@ -19,17 +19,17 @@ from zope.interface import provider
 class ISmartLinkExtension(model.Schema):
 
     # form.widget(internal_link=ContentTreeFieldWidget)
-    internal_link = RelationChoice(
-        title=_(u'label_smartlink_internallink', default='Internal link'),
-        description=_(u'help_smartlink_internallink',
-                      default=u"Browse to find the internal page to which you wish to link. "
-                      u"If this field is used, then any entry in the external link field will be ignored. "
-                      u"You cannot have both an internal and external link."),
-        required=False,
-        vocabulary='plone.app.vocabularies.Catalog'
-    )
-    directives.widget(
-        'internal_link',
-        RelatedItemsFieldWidget,
-        vocabulary='plone.app.vocabularies.Catalog'
-    )
+    # internal_link = RelationChoice(
+    #     title=_(u'label_smartlink_internallink', default='Internal link'),
+    #     description=_(u'help_smartlink_internallink',
+    #                   default=u"Browse to find the internal page to which you wish to link. "
+    #                   u"If this field is used, then any entry in the external link field will be ignored. "
+    #                   u"You cannot have both an internal and external link."),
+    #     required=False,
+    #     vocabulary='plone.app.vocabularies.Catalog'
+    # )
+    # directives.widget(
+    #     'internal_link',
+    #     RelatedItemsFieldWidget,
+    #     vocabulary='plone.app.vocabularies.Catalog'
+    # )
