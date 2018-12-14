466963d9beea50379e1d74e37237ccf20cbf5c20
Giacomo Monari <arsenico13@hotmail.it>
2018-12-14 15:54:48 +0100

    [fix] if the link is broken and the user can't edit, raise 404

--- src/collective/smartlink/browser/view.py
+++ src/collective/smartlink/browser/view.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from plone.app.contenttypes.browser.link_redirect_view import LinkRedirectView as Base  # noqa
 from plone.app.contenttypes.browser.link_redirect_view import NON_RESOLVABLE_URL_SCHEMES  # noqa
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone import api
+from zExceptions import NotFound
 
 
 class LinkRedirectView(Base):
@@ -11,6 +13,8 @@ class LinkRedirectView(Base):
     def absolute_target_url(self):
         """Compute the absolute target URL."""
         url = self.url()
+        mtool = getToolByName(self.context, 'portal_membership')
+        can_edit = mtool.checkPermission('Modify portal content', self.context)
 
         if self.context.remoteUrl.startswith('$'):
             # searching for the linked object
@@ -20,6 +24,9 @@ class LinkRedirectView(Base):
                 return obj.absolute_url()
             else:
                 # Broken Link
-                return None
+                if can_edit:
+                    return None
+                else:
+                    raise NotFound()
         else:
             return super(LinkRedirectView, self).absolute_target_url()
