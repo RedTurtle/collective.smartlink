13f23b35931458c7d335d424a609416449bd179e
Giacomo Monari <arsenico13@hotmail.it>
2019-03-14 10:16:49 +0100

    Preparing release 1.0.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@ Changelog
 =========
 
 
-1.0.1 (unreleased)
+1.0.1 (2019-03-14)
 ------------------
 
 - If the link points to a deleted or an unpublished resource, you get redirected
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='collective.smartlink',
-    version='1.0.1.dev0',
+    version='1.0.1',
     description="An add-on for Plone 5",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
