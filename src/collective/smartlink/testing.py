a05b3be2adcd6a6458b3d590d7e8b870186fe379
Mauro Amico <mauro.amico@gmail.com>
2025-10-24 15:35:40 +0200

    ci

--- src/collective/smartlink/testing.py
+++ src/collective/smartlink/testing.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
-from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PloneSandboxLayer
-from plone.testing import z2
 
 import collective.smartlink
 
@@ -33,14 +31,4 @@ COLLECTIVE_SMARTLINK_INTEGRATION_TESTING = IntegrationTesting(
 COLLECTIVE_SMARTLINK_FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(COLLECTIVE_SMARTLINK_FIXTURE,),
     name="CollectiveSmartlinkLayer:FunctionalTesting",
-)
-
-
-COLLECTIVE_SMARTLINK_ACCEPTANCE_TESTING = FunctionalTesting(
-    bases=(
-        COLLECTIVE_SMARTLINK_FIXTURE,
-        REMOTE_LIBRARY_BUNDLE_FIXTURE,
-        z2.ZSERVER_FIXTURE,
-    ),
-    name="CollectiveSmartlinkLayer:AcceptanceTesting",
 )
--- src/collective/smartlink/tests/test_setup.py
+++ src/collective/smartlink/tests/test_setup.py
@@ -8,6 +8,12 @@ from plone import api
 import unittest
 
 
+try:
+    from Products.CMFPlone.utils import get_installer
+except ImportError:
+    get_installer = None
+
+
 class TestSetup(unittest.TestCase):
     """Test that collective.smartlink is properly installed."""
 
@@ -16,11 +22,14 @@ class TestSetup(unittest.TestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer["portal"]
-        self.installer = api.portal.get_tool("portal_quickinstaller")
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer["request"])
+        else:
+            self.installer = api.portal.get_tool("portal_quickinstaller")
 
     def test_product_installed(self):
         """Test if collective.smartlink is installed."""
-        self.assertTrue(self.installer.isProductInstalled("collective.smartlink"))
+        self.assertTrue(self.installer.is_product_installed("collective.smartlink"))
 
     def test_browserlayer(self):
         """Test that ICollectiveSmartlinkLayer is registered."""
@@ -36,12 +45,15 @@ class TestUninstall(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer["portal"]
-        self.installer = api.portal.get_tool("portal_quickinstaller")
-        self.installer.uninstallProducts(["collective.smartlink"])
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer["request"])
+        else:
+            self.installer = api.portal.get_tool("portal_quickinstaller")
+        self.installer.uninstall_product("collective.smartlink")
 
     def test_product_uninstalled(self):
         """Test if collective.smartlink is cleanly uninstalled."""
-        self.assertFalse(self.installer.isProductInstalled("collective.smartlink"))
+        self.assertFalse(self.installer.is_product_installed("collective.smartlink"))
 
     def test_browserlayer_removed(self):
         """Test that ICollectiveSmartlinkLayer is removed."""
